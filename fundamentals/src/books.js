820a091637ac1bec6d8fc6617e9ae36af0140a03
Dimitar Hristov <48736509+dhhristov@users.noreply.github.com>
2023-11-18 03:07:07 +0200

    Develop (#2)
    * initial fundamentals commit
    
    * clean up boiler plates, add simple book list
    
    * Update README.md
    
    * clean up boilerplate, add simple book list
    
    * complete fundamentals
    
    ---------
    
    Co-authored-by: Dimitar Hristov <dimitarhristov@Dimitars-MacBook-Pro.local>

---
+++ fundamentals/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    title: "Fourth Wing (The Empyrean, 1)",
+    author: "Rebecca Yarros",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    title: "The Woman in Me",
+    author: "Britney Spears ",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61BWsc9eGbL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+];
