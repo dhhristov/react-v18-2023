93c28470a20c6ef758b10d46c7a403054d55a1ce
Dimitar Hristov <dimitarhristov@Dimitars-MacBook-Pro.local>
2023-11-26 12:41:02 +0200

    backroads-app initial commit

---
+++ backroads-app/src/components/Footer.js
@@ -0,0 +1,51 @@
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        <li>
+          <a href="#home" className="footer-link">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="footer-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="footer-link">
+            services
+          </a>
+        </li>
+        <li>
+          <a href="#featured" class="footer-link">
+            featured
+          </a>
+        </li>
+      </ul>
+      <ul class="footer-icons">
+        <li>
+          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
+            <i class="fab fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
+            <i class="fab fa-squarespace"></i>
+          </a>
+        </li>
+      </ul>
+      <p class="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"></span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
